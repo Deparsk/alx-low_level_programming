23a4261eeb6aab57f102d1a9d20cd195c3ba6584
Deparsk <okparapaschal5@gmail.com>
2022-10-24 21:51:40 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,25 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string.
- * @s: input string.
- * Return: length of a string.
+ * _strlen - return the length of a string
+ * Owned By Bwave
+ * @s: char to check
+ * Description: this will return the length of a string
+ * Return: 0 is success
  */
-int _strlen(char *s)
-{
-		int count = 0;
 
-while : (*(s + count) != '\0')
-		count++
-	return (count);
+int _strlen(char *s)
+
+{
+
+		int a = 0;
+
+
+
+			for (; *s++;)
+
+						a++;
+
+				return (a);
+
 }
